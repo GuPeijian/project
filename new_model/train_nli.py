7e3ea89e209fbf6ed12042deda4c620efad6ac29
GuPeijian <gpj123@mail.ustc.edu.cn>
2021-01-16 20:19:14 +0800

    20210116|顾培健|更新nli

--- new_model/train_nli.py
+++ new_model/train_nli.py
@@ -19,16 +19,23 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class tokendataset(Dataset):
-    def __init__(self, list_file):
-        self.dataset = list_file
+    def __init__(self, data_file):
+        self.file_path = data_file
+        dataset = []
+        file_raws = 0
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            for _ in f:
+                file_raws += 1
+                dataset.append(_)
+        self.file_raws = file_raws
+        self.dataset = dataset
 
     def __len__(self):
-        return len(self.dataset)
+        return self.file_raws
 
     def __getitem__(self, idx):
-        data = self.dataset[idx]
-        return (data)
-
+        data = json.loads(self.dataset[idx][:-1]) #remove /n
+        return data
 
 def padding_nli_fn(datalist):
     input_ids=[]
@@ -79,11 +86,11 @@ def set_seed(args):
 def main():
     parser = argparse.ArgumentParser()
     # data and model
-    parser.add_argument("--data_path", default='/home/gpj/project/all_data/encode_data/nli_data/train_nli_data.json', type=str, )
+    parser.add_argument("--data_path", default='/home/gpj/project/all_data/nli_data/nli_data.txt', type=str, )
     parser.add_argument("--model_name_or_path", default="/home/gpj/bert-chinese", type=str, )
-    parser.add_argument("--output_dir", default='/home/gpj/project/all_model/nli', type=str, )
+    parser.add_argument("--output_dir", default='/home/gpj/project/all_model/nli_from_scratch', type=str, )
 
-    parser.add_argument("--batch_size", default=16, type=int, )
+    parser.add_argument("--batch_size", default=8, type=int, )
     parser.add_argument("--gradient_accumulation_steps", type=int, default=1, )
     parser.add_argument("--learning_rate", default=3e-5, type=float, help="学习率")
     parser.add_argument("--weight_decay", default=0.01, type=float, help="学习率衰减")
@@ -91,20 +98,20 @@ def main():
     parser.add_argument("--max_grad_norm", default=1.0, type=float, help="梯度裁减值")
     parser.add_argument("--num_train_epochs", default=2, type=int, help="训练epochs次数", )
     parser.add_argument("--warmup_rate", default=0.1, type=int, help="学习率线性预热步数")
-    parser.add_argument("--logging_steps", type=int, default=50, help="每多少步打印日志")
-    parser.add_argument("--log_dir", default="/home/gpj/project/log/nli", type=str, )
+    parser.add_argument("--logging_steps", type=int, default=100, help="每多少步打印日志")
+    parser.add_argument("--log_dir", default="/home/gpj/project/log/nli_from_scratch", type=str, )
     parser.add_argument("--seed", type=int, default=66, help="初始化随机种子")
     parser.add_argument("--max_steps", default=200000, type=int, help="训练的总步数", )
     parser.add_argument("--save_steps", default=5000, type=int, help="保存的间隔steps", )
     parser.add_argument('--local_rank', default=0, type=int, help='node rank for distributed training')
-    parser.add_argument('--gpus', default=2, type=int, help='number of gpus per node')
+    parser.add_argument('--gpus', default=4, type=int, help='number of gpus per node')
     parser.add_argument('--nodes', default=1, type=int, metavar='N')
     parser.add_argument("--overwrite_output_dir", default=False, action="store_true", help="是否覆盖输出文件夹")
     args = parser.parse_args()
 
     os.environ['MASTER_ADDR'] = '127.0.0.1'
     os.environ['MASTER_PORT'] = '6666'
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
     args.world_size = args.gpus * args.nodes
 
     if (
@@ -129,16 +136,12 @@ def main():
     # Set seed
     set_seed(args)
 
-    # load json data
-    with open(args.data_path, 'r')as f:
-        data_file = json.load(f)
-
-    train_dataset = tokendataset(data_file)
+    train_dataset = tokendataset(args.data_path)
     train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
     train_dataloader = DataLoader(train_dataset, num_workers=0, batch_size=args.batch_size, shuffle=False,
                                   drop_last=True, collate_fn=padding_nli_fn, sampler=train_sampler)
 
-    model = BertForSequenceClassification.from_pretrained(args.model_name_or_path,num_labels=3)
+    model = BertForSequenceClassification.from_pretrained(args.model_name_or_path,num_labels=2)
     tokenizer = BertTokenizer.from_pretrained(args.model_name_or_path)
     SPECIAL_TOKENS_DICT = {'additional_special_tokens': ["<user1>", "<user2>"]}
     tokenizer.add_special_tokens(SPECIAL_TOKENS_DICT)
@@ -148,7 +151,8 @@ def main():
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
                                                       output_device=args.local_rank, find_unused_parameters=True)
     t_total = args.max_steps
-    tb_writer = SummaryWriter(args.log_dir)
+    if args.local_rank == 0:
+        tb_writer = SummaryWriter(args.log_dir)
     no_decay = ['bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
         {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
@@ -166,8 +170,9 @@ def main():
     model.zero_grad()
     if args.local_rank == 0:
         logger.info("start_training")
-
+    current_epoch = 0
     for epoch in trange(int(args.num_train_epochs), desc='Epoch'):
+        current_epoch+=1
         for step, batch in enumerate(tqdm(train_dataloader, desc='Iteration')):
             input_ids, attention_mask,token_type_ids,labels = \
                 batch["input_ids"],batch["attention_mask"],batch["token_type_ids"],batch["labels"]
@@ -199,7 +204,7 @@ def main():
 
                         for key, value in logs.items():
                             tb_writer.add_scalar(key, value, global_step)
-                        #print(json.dumps({**logs, **{"step": global_step}}))
+                        print(json.dumps({**logs, **{"step": global_step}}))
 
                 if args.save_steps > 0 and global_step % args.save_steps == 0 and torch.distributed.get_rank() == 0:
                     output_dir = os.path.join(args.output_dir, 'checkpoint-{}'.format(global_step))
@@ -212,8 +217,15 @@ def main():
                 break
         if args.max_steps > 0 and global_step > t_total:
             break
-
-    tb_writer.close()
+        if torch.distributed.get_rank() == 0:
+            if current_epoch<args.num_train_epochs:
+                output_dir = os.path.join(args.output_dir, 'checkpoint-epoch-{}'.format(current_epoch))
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+                model_to_save = (model.module if hasattr(model, "module") else model)
+                model_to_save.save_pretrained(output_dir)
+    if args.local_rank == 0:
+        tb_writer.close()
     if args.local_rank == 0:
         logger.info(" global_step = %s, average loss = %s", global_step, tr_loss / global_step)
     if not os.path.exists(args.output_dir):
